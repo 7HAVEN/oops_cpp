374d00e2ab80043dd019085b86641d6e7227ea69
7HAVEN <rana68628@gmail.com>
2021-09-26 20:14:03 +0530

    modified prime.cpp

--- Practical-02/prime.cpp
+++ Practical-02/prime.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 bool isPrime(int num){
-    if(n==1) return false;
+    if(num==1) return false;
     for(int i = 2;i*i<num;i++){
         if(num % i == 0 && num != i)  return false; 
     }
